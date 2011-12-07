7d92b707de10810e314d8c1f6b14e4045622da6c
alexandresalome <alexandre.salome@gmail.com>
2011-12-07 22:08:08 +0100

    Remove obsolete tests

--- src/Gitonomy/Bundle/FrontendBundle/Tests/Controller/MainControllerTest.php
+++ src/Gitonomy/Bundle/FrontendBundle/Tests/Controller/MainControllerTest.php
@@ -16,21 +16,4 @@ class MainControllerTest extends WebTestCase
         $this->assertEquals(200, $response->getStatusCode());
         $this->assertEquals('Welcome to Gitonomy.sample!', $crawler->filter('h1')->text());
     }
-
-    public function testDashboard()
-    {
-        $client = self::createClient();
-
-        $client->connect('alice');
-
-        $crawler  = $client->request('GET', '/');
-        $response = $client->getResponse();
-
-        $this->assertEquals(200, $response->getStatusCode());
-        $this->assertEquals('My repositories', $crawler->filter('h2')->eq(1)->text());
-
-        $repositoryGitonomy = $crawler->filter('.well')->eq(0);
-        $this->assertEquals('alice/foobar', $repositoryGitonomy->filter('h3 a')->text());
-        $this->assertEquals('The Foo Bar project', $repositoryGitonomy->filter('p')->text());
-    }
 }
