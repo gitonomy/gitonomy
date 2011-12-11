1ff398829529fb53a77293082d6b3753e62c1192
alexandresalome <alexandre.salome@gmail.com>
2011-12-11 14:42:16 +0100

    Better parsing of commits

--- src/Gitonomy/Git/Parser/CommitParser.php
+++ src/Gitonomy/Git/Parser/CommitParser.php
@@ -14,8 +14,37 @@ class CommitParser
     public $committerDate;
     public $message;
 
+    protected $cursor;
+    protected $content;
+
     public function parse($content)
     {
+        $this->cursor  = 0;
+        $this->content = $content;
+
+        $this->consume('tree ');
+        $this->tree = $this->consumeHash();
+        $this->consumeNewLine();
+
+        $this->parents = array();
+        while ($this->expects('parent ')) {
+            $this->parents[] = $this->consumeHash();
+            $this->consumeNewLine();
+        }
+
+        $this->consume('author ');
+        list($this->authorName, $this->authorEmail, $this->authorDate) = $this->consumeNameEmailDate();
+        $this->authorDate = $this->parseDate($this->authorDate);
+        $this->consumeNewLine();
+
+        $this->consume('committer ');
+        list($this->committerName, $this->committerEmail, $this->committerDate) = $this->consumeNameEmailDate();
+        $this->committerDate = $this->parseDate($this->committerDate);
+        $this->consumeNewLine();
+
+        $this->consumeNewLine();
+        $this->message = $this->consumeAll();
+
         $pattern = '/'.
             'tree (?<tree>[A-Za-z0-9]{40})'.
             "\n".
@@ -28,20 +57,66 @@ class CommitParser
             '(?<message>[^$]*)'.
             '/'
         ;
+    }
 
-        if (!preg_match($pattern, $content, $vars)) {
-            throw new \RuntimeException('Unable to parse a commit');
+    private function consumeAll()
+    {
+        $rest = mb_substr($this->content, $this->cursor);
+        $this->cursor += mb_strlen($rest);
+
+        return $rest;
+    }
+
+    private function consumeNameEmailDate()
+    {
+        if (!preg_match('/(([^\n]*) <([^\n]*)> (\d+ [+-]\d{4}))/A', $this->content, $vars, 0, $this->cursor)) {
+            throw new \RuntimeException('Unable to parse name, email and date');
         }
 
-        $this->tree           = $vars['tree'];
-        $this->parents        = array($vars['parent']);
-        $this->authorName     = $vars['author_name'];
-        $this->authorEmail    = $vars['author_email'];
-        $this->authorDate     = $this->parseDate($vars['author_date']);
-        $this->committerName  = $vars['committer_name'];
-        $this->committerEmail = $vars['committer_email'];
-        $this->committerDate  = $this->parseDate($vars['committer_date']);
-        $this->message        = $vars['message'];
+        $this->cursor += mb_strlen($vars[1]);
+
+        return array($vars[2], $vars[3], $vars[4]);
+    }
+
+    private function expects($expected)
+    {
+        $length = mb_strlen($expected);
+        $actual = mb_substr($this->content, $this->cursor, $length);
+        if ($actual !== $expected) {
+            return false;
+        }
+
+        $this->cursor += $length;
+
+        return true;
+    }
+
+    private function consumeHash()
+    {
+        if (!preg_match('/([A-Za-z0-9]{40})/A', $this->content, $vars, null, $this->cursor)) {
+            throw new \RuntimeException('No hash found');
+        }
+
+        $this->cursor += 40;
+
+        return $vars[1];
+    }
+
+    private function consume($expected)
+    {
+        $length = mb_strlen($expected);
+        $actual = mb_substr($this->content, $this->cursor, $length);
+        if ($actual !== $expected) {
+            throw new \RuntimeException(sprintf('Expected "%s", but got "%s"', $expected, $actual));
+        }
+        $this->cursor += $length;
+
+        return $expected;
+    }
+
+    private function consumeNewLine()
+    {
+        return $this->consume("\n");
     }
 
     protected function parseDate($text)
