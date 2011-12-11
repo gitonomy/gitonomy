b3210e750c89d4bd272bf36a42b3515d18aeb491
alexandresalome <alexandre.salome@gmail.com>
2011-12-11 01:33:41 +0100

    Change put-gitonomy.sh script

--- put-gitonomy.sh
+++ put-gitonomy.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rm -Rf app/cache/repositories/projects/foobar.git
-cp -R .git app/cache/repositories/projects/foobar.git
-
+./app/console gitonomy:project-create Gitonomy gitonomy
+if [ -d app/cache/repositories/projects/gitonomy.git ]; then
+    rm -Rf app/cache/repositories/projects/gitonomy.git
+fi
+mkdir -p app/cache/repositories/projects
+cp -R .git app/cache/repositories/projects/gitonomy.git
