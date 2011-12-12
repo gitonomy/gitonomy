2c0127635ce74db97755055dc273d93793149125
alexandresalome <alexandre.salome@gmail.com>
2011-12-13 00:03:35 +0100

    Add a test for login

--- src/Gitonomy/Bundle/FrontendBundle/Tests/Controller/UserControllerTest.php
+++ src/Gitonomy/Bundle/FrontendBundle/Tests/Controller/UserControllerTest.php
@@ -4,6 +4,9 @@ namespace Gitonomy\Bundle\FrontendBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
+/**
+ * @todo Find a way to test the project with mode "unregistered"
+ */
 class UserControllerTest extends WebTestCase
 {
     protected $client;
@@ -52,5 +55,24 @@ class UserControllerTest extends WebTestCase
 
         $this->assertEquals(1, $node->count());
         $this->assertEquals('Your account was created!', $node->text());
+    }
+
+    public function testLogin()
+    {
+        $crawler = $this->client->request('GET', '/en_US/login');
+
+        $form = $crawler->filter('input[type=submit][value=Login]')->form(array(
+            '_username' => 'foo',
+            '_password' => 'bar'
+        ));
+
+        $this->client->submit($form);
+
+        $this->assertTrue($this->client->getResponse()->isRedirect('http://localhost/en_US/login'));
+
+        $crawler = $this->client->followRedirect();
+
+        $this->assertEquals('Unable to login:', $crawler->filter('.alert-message strong')->text());
+        $this->assertEquals(1, $crawler->filter('.topbar a:contains("Login")')->count());
     }
 }
