e2850553fb76f5dff2cfdfca642ad74e4cb6f739
alexandresalome <alexandre.salome@gmail.com>
2011-12-29 11:10:00 +0100

    Add tests for push/pull on projects

--- src/Gitonomy/Bundle/CoreBundle/Tests/Command/GitCommandTest.php
+++ src/Gitonomy/Bundle/CoreBundle/Tests/Command/GitCommandTest.php
@@ -36,10 +36,91 @@ class GitCommandTest extends CommandTestCase
             ->will($this->returnValue(null))
         ;
 
-        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=1 alice');
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no alice');
 
         $this->assertContains('You are identified as alice', $return);
         $this->assertRegexp('/Foobar[ ]+Developer[ ]/', $return);
         $this->assertRegexp('/Barbaz[ ]+Lead developer[ ]/', $return);
+    }
+
+    public function testIllegalAction()
+    {
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('getOriginalCommand')
+            ->will($this->returnValue('git-bla-pack \'foobar.git\''))
+        ;
+
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no alice');
+
+        $this->assertContains('Action seems illegal', $return);
+    }
+
+    public function testPullNonAuthorizedProject()
+    {
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('getOriginalCommand')
+            ->will($this->returnValue('git-upload-pack \'barbaz.git\''))
+        ;
+
+        $this->shellHandler
+            ->expects($this->never())
+            ->method('handle')
+        ;
+
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no bob');
+
+        $this->assertContains('You are not allowed', $return);
+    }
+
+    public function testPullAuthorizedProject()
+    {
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('getOriginalCommand')
+            ->will($this->returnValue('git-upload-pack \'foobar.git\''))
+        ;
+
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('handle')
+        ;
+
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no bob');
+    }
+
+    public function testPushNonAuthorizedProject()
+    {
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('getOriginalCommand')
+            ->will($this->returnValue('git-receive-pack \'barbaz.git\''))
+        ;
+
+        $this->shellHandler
+            ->expects($this->never())
+            ->method('handle')
+        ;
+
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no bob');
+
+        $this->assertContains('You are not allowed', $return);
+    }
+
+    public function testPushAuthorizedProject()
+    {
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('getOriginalCommand')
+            ->will($this->returnValue('git-receive-pack \'foobar.git\''))
+        ;
+
+        $this->shellHandler
+            ->expects($this->once())
+            ->method('handle')
+        ;
+
+        $return = $this->runCommand($this->client, 'gitonomy:git --stderr=no bob');
     }
 }
