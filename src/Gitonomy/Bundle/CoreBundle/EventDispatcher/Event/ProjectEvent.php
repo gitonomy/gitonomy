7e30071c71b5c560ca4c83dffbae7a85fbfe973b
alexandresalome <alexandre.salome@gmail.com>
2012-01-02 22:25:22 +0100

    Add the User to ProjectEvent

--- src/Gitonomy/Bundle/CoreBundle/EventDispatcher/Event/ProjectEvent.php
+++ src/Gitonomy/Bundle/CoreBundle/EventDispatcher/Event/ProjectEvent.php
@@ -5,18 +5,31 @@ namespace Gitonomy\Bundle\CoreBundle\EventDispatcher\Event;
 use Symfony\Component\EventDispatcher\Event;
 
 use Gitonomy\Bundle\CoreBundle\Entity\Project;
+use Gitonomy\Bundle\CoreBundle\Entity\User;
 
 class ProjectEvent extends Event
 {
     protected $project;
+    protected $user;
 
-    function __construct(Project $project)
+    function __construct(Project $project, User $user = null)
     {
         $this->project = $project;
+        $this->user    = $user;
     }
 
     public function getProject()
     {
         return $this->project;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    public function hasUser()
+    {
+        return null !== $this->user;
     }
 }
