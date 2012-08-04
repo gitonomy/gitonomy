482e9102ac0e04227d6537c40f49d6b2e5a5b691
alexandresalome <alexandre.salome@gmail.com>
2012-08-04 14:18:24 +0200

    Update dependencies

--- app/autoload.php
+++ app/autoload.php
@@ -2,7 +2,7 @@
 
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
-$loader = include __DIR__.'/../vendor/.composer/autoload.php';
+$loader = include __DIR__.'/../vendor/autoload.php';
 
 // intl
 if (!function_exists('intl_get_error_code')) {
