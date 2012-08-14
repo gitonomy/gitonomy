cc162a34c9ef6e3ab65e2776d0fb8776b72c935a
alexandresalome <alexandre.salome@gmail.com>
2012-08-15 00:36:51 +0200

    Finish pagination of commits + Remove a test script

---
+++ src/Gitonomy/Component/Pagination/Adapter/GitLogAdapter.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Gitonomy\Component\Pagination\Adapter;
+
+use Gitonomy\Component\Pagination\PagerAdapterInterface;
+use Gitonomy\Git\Log;
+
+class GitLogAdapter implements PagerAdapterInterface
+{
+    private $log;
+
+    public function __construct(Log $log)
+    {
+        $this->log = $log;
+    }
+
+    public function get($offset, $limit)
+    {
+        $this->log->setOffset($offset);
+        $this->log->setLimit($limit);
+
+        return $this->log->getCommits();
+    }
+
+    public function count()
+    {
+        return $this->log->countCommits();
+    }
+}
