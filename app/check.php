3e96b839555a9e8e335cfd87f81e53ab40be44e7
alexandresalome <alexandre.salome@gmail.com>
2012-10-09 19:50:19 +0200

    cleanup check file

--- app/check.php
+++ app/check.php
@@ -1,10 +1,25 @@
 <?php
 
+/**
+ * This file is part of Gitonomy.
+ *
+ * (c) Alexandre Salomé <alexandre.salome@gmail.com>
+ * (c) Julien DIDIER <genzo.wm@gmail.com>
+ *
+ * This source file is subject to the GPL license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 require_once __DIR__.'/../vendor/autoload.php';
 
-$requirement = new Gitonomy\Component\Requirements\GitonomyRequirements();
+use Gitonomy\Component\Requirements\GitonomyRequirements;
+
+$requirement = new GitonomyRequirements();
+
 if ($requirement->isValid()) {
     echo "All indicators are in the green, looks fine\n";
+    echo "\n";
+    echo "Please web-access file install.php in a browser\n";
 
     return;
 }
