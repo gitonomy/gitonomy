5e515c3298aef9af10f030d8ccca08756d006c39
alexandresalome <alexandre.salome@gmail.com>
2012-10-13 14:16:22 +0200

    install.php and app_dev.php are secured

--- web/install.php
+++ web/install.php
@@ -1,3 +1,27 @@
+<?php
+
+/**
+ * This file is part of Gitonomy.
+ *
+ * (c) Alexandre Salomé <alexandre.salome@gmail.com>
+ * (c) Julien DIDIER <genzo.wm@gmail.com>
+ *
+ * This source file is subject to the GPL license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if ($_SERVER['REMOTE_ADDR'] !== '127.0.0.1' && $_SERVER['REMOTE_ADDR'] !== '::1') {
+    die;
+}
+
+require_once __DIR__.'/../app/bootstrap.php.cache';
+
+use Symfony\Component\HttpFoundation\Request;
+use Gitonomy\Component\Requirements\WebGitonomyRequirements;
+
+$requirements = new WebGitonomyRequirements();
+
+?>
 <!doctype html>
 <html>
     <head>
@@ -10,13 +34,6 @@
             </header>
             <section>
                 <?php
-                    require_once __DIR__.'/../app/bootstrap.php.cache';
-
-                    use Symfony\Component\HttpFoundation\Request;
-                    use Gitonomy\Component\Requirements\WebGitonomyRequirements;
-
-                    $requirements = new WebGitonomyRequirements();
-
                     if ($requirements->isValid()) {
                         echo '<p>Everything is OK</p>';
                         echo '<p class="welcome-buttons"><a class="btn" href="app_dev.php/install/welcome">Continue &raquo;</a>.</p>';
