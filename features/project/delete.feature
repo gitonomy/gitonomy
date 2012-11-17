14b8867adc1a2795d0eae10a9dc338cd673f0140
alexandresalome <alexandre.salome@gmail.com>
2012-11-17 02:32:31 +0100

    tests for delete project feature

---
+++ features/project/delete.feature
@@ -0,0 +1,15 @@
+Feature: A user can delete a project
+    As a user
+    I have possibility to delete repositories
+    So I can remove old or obsolete projects
+
+    Scenario:
+        Given project "test-delete" exists
+          And user "alice" is "Lead developer" on "test-delete"
+          And I am connected as "alice"
+
+         When I am on "/projects/test-delete/admin"
+         Then I should see "Yes, I want to delete test-delete"
+
+         When I click on "Yes, I want to delete test-delete"
+         Then I should see "Project removed"
--- src/Gitonomy/QA/Context/ApiContext.php
+++ src/Gitonomy/QA/Context/ApiContext.php
@@ -7,7 +7,9 @@ use Behat\Behat\Exception\PendingException;
 use Behat\Gherkin\Node\TableNode;
 
 use Gitonomy\QA\KernelFactory;
+use Gitonomy\Bundle\CoreBundle\Entity\Project;
 use Gitonomy\Bundle\CoreBundle\Entity\User;
+use Gitonomy\Bundle\CoreBundle\Entity\UserRoleProject;
 use Gitonomy\Bundle\CoreBundle\Entity\UserSshKey;
 use Gitonomy\Bundle\CoreBundle\EventDispatcher\GitonomyEvents;
 use Gitonomy\Bundle\CoreBundle\EventDispatcher\Event\ProjectEvent;
@@ -134,6 +136,55 @@ class ApiContext extends BehatContext
                 $key->setContent($content);
             }
 
+            $em->flush();
+        });
+    }
+
+    /**
+     * @Given /^project "([^"]*)" exists$/
+     */
+    public function projectExists($slug)
+    {
+        $this->kernelFactory->run(function ($kernel) use ($slug) {
+            $em = $kernel->getContainer()->get('doctrine')->getEntityManager();
+            $project = $em->getRepository('GitonomyCoreBundle:Project')->findOneBySlug($slug);
+            if ($project) {
+                $em->remove($project);
+                $em->flush();
+                $kernel->getContainer()->get('gitonomy_core.event_dispatcher')->dispatch(GitonomyEvents::PROJECT_DELETE, new ProjectEvent($project));
+            }
+
+            $project = new Project($slug, $slug);
+            $em->persist($project);
+            $em->flush();
+            $kernel->getContainer()->get('gitonomy_core.event_dispatcher')->dispatch(GitonomyEvents::PROJECT_CREATE, new ProjectEvent($project));
+        });
+    }
+
+    /**
+     * @Given /^user "([^"]*)" is "([^"]*)" on "([^"]*)"$/
+     */
+    public function userIsOn($username, $role, $project)
+    {
+        $this->kernelFactory->run(function ($kernel) use ($username, $role, $project) {
+            $em = $kernel->getContainer()->get('doctrine')->getEntityManager();
+
+            $user    = $em->getRepository('GitonomyCoreBundle:User')->findOneByUsername($username);
+            $role    = $em->getRepository('GitonomyCoreBundle:Role')->findOneByName($role);
+            $project = $em->getRepository('GitonomyCoreBundle:Project')->findOneByName($project);
+
+            $existing = $em->getRepository('GitonomyCoreBundle:UserRoleProject')->findOneBy(array(
+                'user'    => $user,
+                'project' => $project
+            ));
+
+            if ($existing) {
+                $em->remove($existing);
+            }
+            $em->flush();
+
+            $userRole = new UserRoleProject($user, $project, $role);
+            $em->persist($userRole);
             $em->flush();
         });
     }
