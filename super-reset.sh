192abbcfb457f89f0a21153fb5e8dae370e1e873
alexandresalome <alexandre.salome@gmail.com>
2013-01-07 00:40:42 +0100

    fix super-reset

--- super-reset.sh
+++ super-reset.sh
@@ -5,12 +5,12 @@ cd `php -r "echo dirname(realpath('$0'));"`
 ./reset.sh
 
 echo ">>> Creating user in Gitonomy"
-./app/console gitonomy:user-create user user 'user@example.org' "User"
-./app/console gitonomy:user-role-create user  "Project creator"
+php app/console gitonomy:user-create user user 'user@example.org' "User"
+php app/console gitonomy:user-role-create user  "Project creator"
 
 if [ -f ~/.ssh/id_rsa.pub ]; then
     echo ">>> Adding your SSH key in Gitonomy"
-    ./app/console gitonomy:user-ssh-key-create user "SSH key from system" "`cat ~/.ssh/id_rsa.pub`"
+    php app/console gitonomy:user-ssh-key-create user "SSH key from system" "`cat ~/.ssh/id_rsa.pub`"
 fi
 
 if [ -f ~/.ssh/authorized_keys -a ! -f ~/.ssh/authorized_keys.src ]; then
@@ -20,7 +20,7 @@ fi
 
 if [ -f ~/.ssh/authorized_keys ]; then
     echo ">>> Replacing authorized_keys"
-    ./app/console gitonomy:authorized-keys -i | tee ~/.ssh/authorized_keys
+    php app/console gitonomy:authorized-keys -i | tee ~/.ssh/authorized_keys
 fi
 
 if [ -f ~/.ssh/authorized_keys.src ]; then
@@ -28,14 +28,12 @@ if [ -f ~/.ssh/authorized_keys.src ]; then
     cat ~/.ssh/authorized_keys.src | tee -a ~/.ssh/authorized_keys
 fi
 
-./app/console gitonomy:project-create Symfony symfony
-./app/console gitonomy:project-create Gitonomy gitonomy
+php app/console gitonomy:project-create Symfony symfony
+php app/console gitonomy:project-create Gitonomy gitonomy
 
 for project in gitonomy symfony; do
     echo ">>> Adding user $USER as lead developer on project $project"
-    ./app/console gitonomy:git-access create $project lead-dev "*" 1 1
-    ./app/console gitonomy:user-role-create user  "Lead developer" $project
-    ./app/console gitonomy:user-role-create admin "Lead developer" $project
+    php app/console gitonomy:user-role-create user  "Lead developer" $project
 done
 
 export GITONOMY_USER="user"
